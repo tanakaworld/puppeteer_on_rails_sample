ea40b14170505a73743dddacdc1fed129d565e1a
tanakaworld <yutaro.tanaka.world@gmail.com>
2019-06-20 16:28:17 +0900

    feat: make article able to upload cover image

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  validates :cover_image, presence: true
+
+  has_one_attached :cover_image
 end
